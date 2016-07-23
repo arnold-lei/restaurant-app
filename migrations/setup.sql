917fe475d7fbb6c839a7723ea5c9c10ff68069f0
Arnold Lei <arnold.lei88@gmail.com>
2016-07-23 13:53:33 -0400

    Added migration

---
+++ migrations/setup.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE restaurant_db;
+
+USE restaurant_db;
+
+CREATE TABLE reservations(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NULL,
+  phone INT(10) NULL,
+  email VARCHAR(100) NULL,
+  unique_res VARCHAR(100) NULL,
+  PRIMARY KEY(id)
+);
