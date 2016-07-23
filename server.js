c856e0aab70159a8367188beda6e551c78dd4d43
adam fader <adamfader@gmail.com>
2016-07-23 12:27:32 -0400

    added initial mysql connection and select all query

--- server.js
+++ server.js
@@ -14,6 +14,30 @@ var reservations = [];
 
 var waitlist = [{}];
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "sector001",
+  database: "restaurant_db"
+})
+
+connection.connect(function(err) {
+  if(err) throw err;
+  console.log("connected as id " + connection.threadId);
+})
+
+var query = 'SELECT * FROM reservations';
+ 
+  connection.query(query, function(err, rows, fields) {
+    if (err) throw err;
+
+    for (var i in rows) {
+        console.log('Product ID: ', rows[i].id, ' Name: ', rows[i].name, ' Phone: ', rows[i].phone, ' Email: ', rows[i].email, ' Unique: ', rows[i].unique_res);
+    }
+  });
+
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'index.html'));
 })
